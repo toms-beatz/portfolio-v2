3958de1574bee402c471d9a000fc9bf4947af887
toms412 <thomas.mazeau@icloud.com>
2025-02-17 21:44:15 +0100

    Fix: unused var

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import { Freeman } from "next/font/google";
 import "./globals.css";
-
-const freeman: Freeman = ({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-freeman'
-});
 
 export const metadata: Metadata = {
   title: "Thomas Mazeau - Portfolio",
@@ -19,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={freeman.variable}>
+    <html lang="en">
       <body
         className={`antialiased dark:bg-dark-1 bg-light-1`}
       >
