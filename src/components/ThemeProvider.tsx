004a93cc2446523e51ccb83634caa4007c740d2a
toms412 <thomas.mazeau@icloud.com>
2025-02-17 16:21:00 +0100

    Fix: lightmode flash

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,38 +1,44 @@
-import { Moon, Sun } from 'lucide-react'; // Import icons
-import { useEffect, useState } from 'react';
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function ThemeProvider() {
-    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+    const [theme, setTheme] = useState<'light' | 'dark' | null>(null);
 
     useEffect(() => {
-        const savedTheme = localStorage.getItem('theme');
-        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (typeof window !== "undefined") {
+            const savedTheme = localStorage.getItem("theme");
+            const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-        if (savedTheme) {
-            setTheme(savedTheme as 'light' | 'dark');
-        } else if (systemPrefersDark) {
-            setTheme('dark');
+            if (savedTheme) {
+                setTheme(savedTheme as "light" | "dark");
+            } else {
+                setTheme("light"); // Light mode par défaut
+            }
         }
     }, []);
 
     useEffect(() => {
-        const root = document.documentElement;
-
-        if (theme === 'dark') {
-            root.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            root.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
+        if (theme) {
+            const root = document.documentElement;
+            if (theme === "dark") {
+                root.classList.add("dark");
+            } else {
+                root.classList.remove("dark");
+            }
+            localStorage.setItem("theme", theme);
         }
     }, [theme]);
 
+    if (theme === null) return null;
+
     return (
         <button
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             className="p-2 transition rounded text-light-5 dark:text-white"
         >
-            {theme === 'dark' ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
+            {theme === "dark" ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
         </button>
     );
 }
