c0568b9a9b95ca09897d260c2610219557793d16
toms412 <thomas.mazeau@icloud.com>
2025-02-19 19:14:13 +0100

    Add: Gtag for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Script from "next/script";
 config.autoAddCss = false
 
 export const metadata: Metadata = {
@@ -16,6 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZL0V3DH7TY"></Script>
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZL0V3DH7TY');
+          `}
+        </Script>
+      </head>
       <body
         className={`antialiased dark:bg-dark-1 bg-light-1`}
       >
