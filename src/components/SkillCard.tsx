617af10c36094887a35a61f62cafaff8edf025ec
toms412 <thomas.mazeau@icloud.com>
2025-02-19 16:47:53 +0100

    Fix: SkillCard alignment

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -11,7 +11,7 @@ interface SkillCardProps {
 
 const SkillCard: React.FC<SkillCardProps> = ({ icon, title }) => {
     return (
-        <div className="flex flex-row gap-6 xl:justify-center items-center p-4 before:content-[''] before:absolute before:[border-radius:inherit] rounded-xl bg-light-1 dark:bg-dark-2">
+        <div className="flex flex-row gap-6 xl:justifcenter items-center p-4 before:content-[''] before:absolute before:[border-radius:inherit] rounded-xl bg-light-1 dark:bg-dark-2 pr-auto">
             <FontAwesomeIcon icon={icon} className="text-light-4" size="2xl" />
             <div className="flex flex-col gap-2">
                 <h3 className="text-light-4 font-semibold">{title}</h3>
